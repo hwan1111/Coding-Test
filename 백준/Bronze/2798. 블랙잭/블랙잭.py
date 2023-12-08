85421a5947aa04894f41ef8f4e99255a088a6da9
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2023-12-08 20:18:00 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+cards = list(map(int, input().split()))
+result = 0
+
+for i in range(N-2):
+    for j in range(i+1, N-1):
+        for k in range(j+1, N):
+            total = cards[i] + cards[j] + cards[k]
+        
+            if result < total <= M:
+                result = total
+
+print(result)
