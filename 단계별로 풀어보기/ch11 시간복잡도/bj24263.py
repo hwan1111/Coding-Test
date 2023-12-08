03dfd00a5e88a65be514ce25a1d1195ae1d93d85
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2023-12-08 19:12:06 +0900

    시간복잡도

---
+++ 단계별로 풀어보기/ch11 시간복잡도/bj24263.py
@@ -0,0 +1,16 @@
+#24263, 알고리즘 수업 - 알고리즘의 수행 시간 2
+#뒤로 갈수록 명령어의 정확한 실행 횟수를 구하기 까다로워질 것입니다. 그러나...
+
+"""
+MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n
+        sum <- sum + A[i]; # 코드1
+    return sum;
+}
+"""
+
+n = int(input())
+
+print(n)
+print(0)
