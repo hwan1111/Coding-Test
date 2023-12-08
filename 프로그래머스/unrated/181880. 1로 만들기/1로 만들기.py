77a5d9a1ca02012b1dbc030e86a1a54929a56fa6
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2023-12-08 22:44:43 +0900

    [level unrated] Title: 1로 만들기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
@@ -1,2 +1,17 @@
 def solution(num_list):
-    return sum(len(bin(i)) - 3 for i in num_list)
+    answer = 0
+    temp = [1]*len(num_list)
+    while temp != num_list:
+        temp = num_list.copy()
+        for i in range(len(num_list)):
+            if num_list[i]%2 == 0:
+                num_list[i] //= 2
+                answer += 1
+            else:
+                if num_list[i] == 1:
+                    pass
+                else:
+                    num_list[i] = (num_list[i]-1)/2
+                    answer += 1
+
+    return answer
