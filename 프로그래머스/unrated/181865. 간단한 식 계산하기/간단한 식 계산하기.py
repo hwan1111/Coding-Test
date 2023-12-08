1d81190e2ad42dd0d83c4213c4174b3f344ac12c
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2023-12-08 22:48:12 +0900

    [level unrated] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -1,1 +1,8 @@
-solution=eval
+def solution(binomial):
+    op = binomial.split()[1]
+    if op == '+':
+            return int(binomial.split()[0])+int(binomial.split()[2])
+    if op == '-':
+            return int(binomial.split()[0])-int(binomial.split()[2])
+    if op == '*':
+            return int(binomial.split()[0])*int(binomial.split()[2])
