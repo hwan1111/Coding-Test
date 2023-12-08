baea540b6d01473bd43fe5bd2cdd4ac334d63b6d
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2023-12-08 20:25:29 +0900

    [Bronze II] Title: 공 바꾸기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,7 @@
+N, M = map(int, input().split())
+basket = [i for i in range(1, N + 1)]
+for _ in range(M) :
+    i, j = map(int, input().split())
+    basket[i-1], basket[j-1] = basket[j-1], basket[i-1]
+for k in range(N) :
+    print(basket[k], end = " ")
