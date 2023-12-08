e5646e1c2b807be191256cef7807163d94319efc
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2023-12-08 20:30:36 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 720 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,11 @@
+a, b, c, d, e, f = map(int, input().split())
+result = []
+
+for x in range(-999, 1000):
+    for y in range(-999, 1000):
+        if a*x + b*y == c and d*x + e*y == f:
+            result = [x, y]
+            break
+
+for i in range(len(result)):
+    print(result[i], end = " ")
