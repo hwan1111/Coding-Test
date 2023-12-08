5bccc58f41d656b9d1bce656101f3623c7fa941c
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2023-12-08 20:16:03 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,8 @@
+T = int(input())
+for _ in range(T):
+    C = int(input())
+    Quarter = C//25
+    Dime = (C-25*Quarter)//10
+    Nickel = (C-25*Quarter-10*Dime)//5
+    Penny = (C-25*Quarter-10*Dime-5*Nickel)
+    print(Quarter, Dime, Nickel, Penny)
