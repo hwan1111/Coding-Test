24414cf1abc497198d5a25e6b8cad93bdc497e3c
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2023-12-10 22:16:53 +0900

    [Silver IV] Title: 수 찾기, Time: 172 ms, Memory: 50388 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,27 +1,9 @@
 import sys
 
 N = int(sys.stdin.readline())
-A = list(map(int, sys.stdin.readline().rstrip().split()))
-A.sort()
-
+A = set(map(int, sys.stdin.readline().rstrip().split()))
 M = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().rstrip().split()))
 
 for num in arr:
-    left = 0
-    right = N-1
-    isExist = False
-    while left <= right:
-        mid = (left + right) // 2
-
-        if num == A[mid]:
-            isExist = True
-            print(1)
-            break
-        elif num > A[mid]:
-            left = mid + 1
-        elif num < A[mid]:
-            right = mid - 1
-    
-    if not isExist:
-        print(0)
+    print(1) if num in A else print(0)
