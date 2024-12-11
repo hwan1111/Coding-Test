9d3d08de1afff92bc43a58b4c17ea9f8f400c29b
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2024-12-11 13:41:09 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,12 +1,9 @@
 def solution(number, k):
     stack = []
     for num in number:
-        while stack and stack[-1] < num:
-            if k > 0:
-                stack.pop()
-                k -= 1
-            else:
-                break
+        while stack and stack[-1] < num and k > 0:
+            stack.pop()
+            k -= 1
                 
         stack.append(num)
         
