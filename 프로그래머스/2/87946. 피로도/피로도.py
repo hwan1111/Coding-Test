ba34b7fcf965e94bef12a8bc87190dbc627a6b98
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2025-03-24 15:43:08 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -0,0 +1,18 @@
+from itertools import permutations
+
+def solution(k, dungeons):
+    answer = 0
+    lst = list(permutations(dungeons))
+    for posible_order in lst:
+        count = 0
+        curr_k = k
+        for min_fatigue, exhaustion_fatigue in posible_order:
+            if curr_k >= min_fatigue:
+                curr_k -= exhaustion_fatigue
+                count += 1
+            else:
+                break
+        
+        answer = max(answer, count)
+
+    return answer
