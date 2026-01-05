872d104802185b2d60be24466b8b846df7fa7081
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2026-01-05 14:13:42 +0900

    [Silver V] Title: 최소공배수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -0,0 +1,10 @@
+def GCD(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def LCM(a, b):
+    return (a * b) // GCD(a, b)
+    
+a, b = map(int, input().split())
+print(LCM(a, b))
