251ceb57ad9e171a5509d0974e033a31585ccfeb
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2026-01-13 16:44:16 +0900

    [Silver IV] Title: 큐 2, Time: 1252 ms, Memory: 143268 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,15 +1,15 @@
-import sys
+# 18258
+# 큐 2
 from collections import deque
+import sys
 
-# 입출력 속도 향상
 input = sys.stdin.readline
 
 def fast_print(x):
-    sys.stdout.write(str(x) + '\n')
+    sys.stdout.write(str(x)+'\n')
 
 queue = deque([])
 
-# 명령어 매핑 (lambda를 사용하여 실행 가능한 함수 형태로 저장)
 user_input_manual = {
     'pop': lambda: queue.popleft() if queue else -1,
     'size': lambda: len(queue),
@@ -18,19 +18,12 @@ user_input_manual = {
     'back': lambda: queue[-1] if queue else -1
 }
 
-N_str = input().strip()
-if N_str:
-    N = int(N_str)
-    for _ in range(N):
-        line = input().split()
-        if not line:
-            continue
-            
-        cmd = line[0]
+N = int(input().strip())
+for _ in range(N):
+    user_input = input().split()
+    cmd = user_input[0]
 
-        if cmd == 'push':
-            queue.append(line[1])
-        else:
-            # 딕셔너리에서 함수를 꺼내 즉시 실행하고 출력
-            result = user_input_manual[cmd]()
-            fast_print(result)
+    if cmd == 'push':
+        queue.append(user_input[1])
+    else:
+        fast_print(user_input_manual[cmd]())
