60752f4e6bd34977d7c53f9cb7dea10997e29acd
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2026-01-14 10:52:00 +0900

    [Silver IV] Title: 카드2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,11 +1,17 @@
-from collections import deque
+import sys
 
-N = int(input())
+N = int(sys.stdin.readline())
 
-stack = deque([i for i in range(1, N + 1)])
-
-while len(stack) > 1:
-    stack.popleft()
-    stack.append(stack.popleft())
-
-print(stack[0])
+if N == 1:
+    print(1)
+else:
+    # N의 비트 길이를 이용해 가장 큰 2의 거듭제곱을 바로 계산
+    # 예: N=6(110(2)) -> bit_length=3 -> 1 << (3-1) = 100(2) = 4
+    highest_power_of_2 = 1 << (N.bit_length() - 1)
+    
+    L = N - highest_power_of_2
+    
+    if L == 0:
+        print(N)
+    else:
+        print(2 * L)
