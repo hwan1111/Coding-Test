c20b614d1ec0b24bdc52e5d1d90d3aec7203fd30
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2026-01-30 09:15:33 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.86 ms, Memory: 28.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,11 +1,8 @@
+import numpy as np
+
 def solution(arr1, arr2):
-    # 결과 행렬 초기화
-    answer = [[0 for _ in range(len(arr2[0]))] for _ in range(len(arr1))]
+    arr1 = np.array(arr1)
+    arr2 = np.array(arr2)
+    answer = arr1 @ arr2
     
-    # 행렬 곱셈
-    for i in range(len(arr1)):
-        for j in range(len(arr2[0])):
-            for k in range(len(arr2)):
-                answer[i][j] += arr1[i][k] * arr2[k][j]
-    
-    return answer
+    return answer.tolist()
