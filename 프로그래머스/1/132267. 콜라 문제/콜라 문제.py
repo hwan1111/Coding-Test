81da02d1495efbb27228ad2292dd3375fb7c7d3f
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2026-05-12 17:31:16 +0900

    [level 1] Title: 콜라 문제, Time: 0.35 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,9 +1,10 @@
 def solution(a, b, n):
     answer = 0
+    
     while n >= a:
         remain = n % a
         n = b * (n//a)
         answer += n
         n += remain
-    
+            
     return answer
